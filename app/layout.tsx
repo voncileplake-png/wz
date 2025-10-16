5b0ff73404a0f6b981c6ae2ad5cb9a4a2eb8b874
voncileplake-png <voncileplake@gmail.com>
2025-10-16 20:32:52 +0800

    速度监测

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Suspense } from "react"
 import "./globals.css"
 
@@ -26,6 +27,9 @@ export default function RootLayout({
         <Suspense fallback={null}>{children}</Suspense>
         <Suspense fallback={null}>
           <Analytics />
+        </Suspense>
+        <Suspense fallback={null}>
+          <SpeedInsights />
         </Suspense>
       </body>
     </html>
